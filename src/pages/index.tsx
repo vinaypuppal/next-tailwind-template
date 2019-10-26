a4daafa4a8e8429f1a3ebba9f07315c6c05479b8
vinaypuppal <me@vinaypuppal.com>
2019-10-27 02:20:58 +0530

    move all inside src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import SEO from '../components/SEO';
+import { H1 } from '../components/h1';
 
 export default () => (
   <>
     <SEO title="next.js + tailwindcss" />
     <main className="max-w-6xl px-4 mx-auto min-h-screen flex items-center justify-center">
-      <h1 className="text-center text-2xl sm:text-3xl md:text-6xl">Next.js + tailwindcss 😍</h1>
+      <H1>Next.js + tailwindcss 😍</H1>
     </main>
   </>
 );
